1d678f33a2b2fad2ca1a902118a7b8387ceaa31b
Lun <lunsan951@gmail.com>
2019-04-14 20:14:58 +0900

    add config.fish

---
+++ config.fish
@@ -0,0 +1,6 @@
+alias les "cd /Users/kamimura/Documents/lesson"
+alias ls "ls -G"
+alias mongod "sudo mongod --dbpath /var/lib/mongodb --logpath /var/log/mongodb.log"
+. (pyenv init - | psub)
+set -x PATH $HOME/.rbenv/shims $PATH
+
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,3 +1,4 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiled/.bashrc ~/.config/fish/config.fish
